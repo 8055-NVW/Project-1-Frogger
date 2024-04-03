cdf68a45d9789bbffdcf668905890b1af0c76aa1
Vivian <vivlopes95@gmail.com>
2024-04-04 00:25:30 +0100

    added to frogger project and refactored some code

--- Frogger/scripts/app.js
+++ Frogger/scripts/app.js
@@ -21,7 +21,7 @@ scoreEl.innerText = score
 // check win variable?
 let win;
 // variable for lives
-let lives = 3;
+let lives = 30;
 livesEl.innerText = lives
 //variable for countdown timer
 let count = 60
@@ -33,9 +33,7 @@ let froggyCurrentPos = froggyStartPos;
 //array for lillypads(end-goal)
 
 // GameBoard constants
-const lillyPadCells = [1,3,5,7]
-
-
+const lillyPadCells = [1, 3, 5, 7]
 
 
 // ? On Page Load
@@ -52,13 +50,8 @@ const lillyPadCells = [1,3,5,7]
 
 
 
-function resetFrogPos() {
-    //remove frog from board
-    cells.forEach(cell => cell.classList.remove('frog'))
-    // reset 
-    froggyCurrentPos = froggyStartPos
-    cells[froggyCurrentPos].classList.add('frog')
-}
+
+
 
 // ? Generate game board
 const cols = 9;
@@ -67,77 +60,108 @@ const cellCount = rows * cols;
 const cells = [];
 
 function makeGameBoard() {
-  for (let index = 0; index < cellCount; index++) {
-    const cell = document.createElement("div");
-    cell.innerText = index;
-    cell.dataset.index = index;
-    cell.classList.add("grid-cell");
-    cell.style.width = `${100 / cols}%`;
-    cell.style.height = `${100 / rows}%`;
-    //Make lillypads
-    if(lillyPadCells.includes(index)) {
-        cell.classList.add('lillypad')
+    for (let index = 0; index < cellCount; index++) {
+        const cell = document.createElement("div");
+        cell.innerText = index;
+        cell.dataset.index = index;
+        cell.classList.add("grid-cell");
+        cell.style.width = `${100 / cols}%`;
+        cell.style.height = `${100 / rows}%`;
+        //Make lillypads
+        if (lillyPadCells.includes(index)) {
+            cell.classList.add('lillypad')
+        }
+        cells.push(cell);
+        gameBoard.append(cell);
     }
-    cells.push(cell);
-    gameBoard.append(cell);
-  }
-  resetFrogPos()
+    resetFrogPos()
+    car()
 }
 
 
 
 // function to handle keypress
 function inputHandler(e) {
+
     cells[froggyCurrentPos].classList.remove('frog')
-    if(e.key === 'ArrowRight' && froggyCurrentPos % cols !== cols - 1) {
+
+    if (e.key === 'ArrowRight' && froggyCurrentPos % cols !== cols - 1) {
         froggyCurrentPos++
     } else if (e.key === 'ArrowLeft' && froggyCurrentPos % cols !== 0) {
         froggyCurrentPos--
+        //Problem Point Sorted :D
     } else if (e.key === 'ArrowUp' && cells[froggyCurrentPos - cols]) {
-        froggyCurrentPos-= cols
+        if (!cells[froggyCurrentPos - cols].classList.contains('home')) {
+            froggyCurrentPos -= cols
+        }
     } else if (e.key === 'ArrowDown' && cells[froggyCurrentPos + cols]) {
-        froggyCurrentPos+= cols
+        froggyCurrentPos += cols
     }
     cells[froggyCurrentPos].classList.add('frog')
-    winHandler()
+    checkWin(froggyCurrentPos)
 }
+
+
+
+// function to reset frogs position
+function resetFrogPos() {
+    //remove frog from board
+    cells.forEach(cell => cell.classList.remove('frog'))
+    // reset 
+    froggyCurrentPos = froggyStartPos
+    cells[froggyCurrentPos].classList.add('frog')
+}
+
 
 
 // function to handle collisions
-//define collision elements in array?
-// sequence through to check if frog class is included in the collision cells
-// if true initiate lose function
-
-// function to handle for win
-function winHandler() {
-    // check to see if frog makes it to .lillyPad
-    const lillyPadArr = document.querySelectorAll('.lillypad')
-    lillyPadArr.forEach(lillyPad => {
-        if(lillyPad.classList.contains('frog')) {  
-            // add to score variable if true
-            score+= 50
-            scoreEl.innerText = score
-            // keep frog class on the .lillypad cell
-
-            // reset position of frog
-            resetFrogPos()
-            console.log(score)
-        }
-    })
-    
+function collisionHandler(currentPos) {
+    if (cells[currentPos].classList.contains('frog')) {
+        console.log('Hit Frog!')
+        lossHandler()
+    }
 }
 
 
 
+//function to check win
+function checkWin(currentPos) {
+    if (cells[currentPos].classList.contains('lillypad')) {
+        console.log('there!')
+        winHandler(cells[currentPos])
+    }
+}
 
 
+
+// function to handle for win(land on lillypad)
+function winHandler(lily) {
+    // add to score variable and update text
+    score += 50
+    scoreEl.innerText = score
+    // keep frog class on the .lillypad cell
+    lily.classList.add('home')
+    // reset position of frog
+    resetFrogPos()
+}
 
 
 // function to handle lose
-//check lives left
-//deduct life if lives remain
-//display final score on lising final life
-//initiate board reset function
+function lossHandler() {
+    //check lives left
+    if (lives > 0) {
+        //deduct life
+        lives--
+        livesEl.innerText = lives
+        resetFrogPos()
+    } else {
+        // complete later
+        //display final score on lising final life
+        //initiate board reset function
+    }
+
+}
+
 
 //function to reset game board
 function gameReset() {
@@ -150,7 +174,8 @@ function gameReset() {
     //reset time
     let count = 60
     countdownEl.innerText = count
-    // set .frog position to starting position
+    //enable start button
+    startGameBtn.disabled = false
 }
 
 
@@ -175,6 +200,25 @@ function gameReset() {
 //water element function
 //--function to create logs
 
+//function to create car
+function car() {
+    const carStartPos = 72
+    let currPos = carStartPos
+    function move() {
+        cells[currPos].classList.remove('car')
+        if (currPos === 63) {
+            currPos = currPos + cols
+        }
+        currPos--
+        collisionHandler(currPos)
+        cells[currPos].classList.add('car')
+    }
+    move()
+    setInterval(move, 100);
+}
+
+
+
 
 
 
@@ -183,23 +227,43 @@ function initializeGame() {
     //initialise grid load on start button click
     makeGameBoard()
 
-    interval = setInterval(()=> {
-        count--
+
+    // function to manage timer interval
+    function startCountdown() {
+        count = 10
         countdownEl.innerText = count
-        document.addEventListener('keyup', inputHandler)
-        if(count < 1) {
-            lives--;
-            livesEl.innerText = lives
-            resetFrogPos()
-            clearInterval(interval)
-            interval = setInterval(intervalFunction, 2000)
+        // function to handel seconds
+        function timer() {
+            count--
+            countdownEl.innerText = count
+            document.addEventListener('keyup', inputHandler)
+
+            if (lives < 1) {
+                clearTimeout(startCountdown)
+                clearInterval(interval)
+                gameReset()
+            }
+            if (count < 1) {
+                //refactored this 
+                lossHandler()
+                // lives--;
+                // livesEl.innerText = lives
+                // resetFrogPos()
+                clearInterval(interval)
+                setTimeout(startCountdown, 1500);
+
+            }
+
         }
-        
-    },500)
+        const interval = setInterval(timer, 1000)
+    }
+    startCountdown()
 }
+
 
 // ? Events
 
 // eventlistener for start-game button click   
-startGameBtn.addEventListener('click',initializeGame)
+startGameBtn.addEventListener('click', initializeGame)
+
 
